941641bd92119ed78e654902b81f3773ebaeb649
Abdelmajeed <shaheen.abdelmajeed:outlook.com>
2020-03-18 13:39:39 +0200

    test_pass

--- src/AuthorsList.js
+++ src/AuthorsList.js
@@ -7,7 +7,7 @@ const AuthorsList = props => {
   const authorCards = props.authors.map(author => (
     <AuthorCard key={author.first_name + author.last_name} author={author} />
   ));
-
+  //asdadasdasdasd
   return (
     <div className="authors">
       <h3>Authors</h3>
